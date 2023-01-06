cb2e963774d1159e4d1738cf782b38b2de596d62
赵超兰 <973180323@qq.com>
2023-01-06 12:13:24 +0800

    tinymce demo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import tinymce from "tinymce/tinymce";
+// import Editor from "@tinymce/tinymce-vue";
+// import "tinymce/icons/default";
+// import "tinymce/themes/silver/theme.min.js";
+// Vue.component('editor', Editor); //注册全局组件
+
 createApp(App).mount('#app')
